e8d8ad71c06ffcd48ca939f86948da302221939b
Andrei Olaru <olaruandreidan@gmail.com>
2023-09-30 20:53:19 +0300

    Add initial level structure

--- lib/data/levels.lua
+++ lib/data/levels.lua
@@ -1,11 +1,23 @@
 return {
   player_start_popularity = 0.45,
-  levels = {{
-    npcs = { "alice", "alice" },
+  levels = {{ --the first one
+    npcs = { "deer", "deer", "frog" },
     duration = 60,
-  }, {
-    npcs = { "alice", "alice", "bob" },
-    duration = 60,
+  }, {--the second one
+    npcs = { "deer", "deer", "horse", "frog", "elephant" },
+    duration = 80,
+  }, {--the third one
+    npcs = { "horse", "frog", "cat", "dog" },
+    duration = 80,
+  }, {-- the fourth one
+    npcs = { "deer", "deer", "deer", "rabbit", "deer" },
+    duration = 80,
+  }, {-- the fifth one
+    npcs = { "cat", "frog", "fox", "dog", "deer", "fox" },
+    duration = 80,
+  }, {-- the finale
+    npcs = { "deer", "dog", "fox", "cat", "frog", "horse", "rabbit", "elephant" },
+    duration = 120,
   }}
 }
 
