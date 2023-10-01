2ca92c48bf73bbc5259d3e2664f07ddd90b21bc3
Andrei Olaru <olaruandreidan@gmail.com>
2023-10-01 20:59:59 +0300

    Adjust difficulty

--- lib/data/levels.lua
+++ lib/data/levels.lua
@@ -1,19 +1,19 @@
 return {
   player_start_popularity = 0.45,
   levels = {{ --the first one
-    npcs = { "deer1", "deer2", "frog1" },
+    npcs = { "deer1", "deer2" },
     duration = 120,
   }, {--the second one
-    npcs = { "deer1", "deer2", "horse1", "frog1", "elephant" },
+    npcs = { "deer1", "deer2", "fox1" },
     duration = 180,
   }, {--the third one
-    npcs = { "horse1", "frog1", "cat1", "dog1" },
+    npcs = { "deer1", "fox1", "fox2" },
     duration = 180,
   }, {-- the fourth one
-    npcs = { "deer1", "deer2", "deer3", "rabbit1", "deer4" },
+    npcs = { "deer1", "deer2", "deer3", "cat1" },
     duration = 180,
   }, {-- the fifth one
-    npcs = { "cat1", "frog1", "fox1", "dog1", "deer1", "fox1" },
+    npcs = { "fox1", "cat1", "cat2", "cat3", "fox2", "fox3" },
     duration = 180,
   }, {-- the finale
     npcs = { "deer1", "dog1", "fox1", "cat1", "frog1", "horse1", "rabbit1", "elephant1" },
