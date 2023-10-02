59675093d9005783b5625697deef54a30ad00781
Andrei Olaru <olaruandreidan@gmail.com>
2023-10-02 11:46:50 +0300

    Add frog character

--- lib/data/levels.lua
+++ lib/data/levels.lua
@@ -1,26 +1,26 @@
 return {
   player_start_popularity = 0.2,
   levels = {{ --the first one
-    npcs = { "deer1", "deer2" },
-    duration = 100,
+    npcs = { "deer1", "frog1" },
+    duration = 90,
   }, {--the second one
     npcs = { "deer1", "deer2", "fox1" },
-    duration = 150,
+    duration = 120,
   }, {--the third one
     npcs = { "deer1", "fox1", "fox2" },
-    duration = 150,
+    duration = 120,
   }, {-- the fourth one
     npcs = { "deer1", "deer2", "deer3", "cat1" },
-    duration = 150,
+    duration = 120,
   }, {-- the fifth one
-    npcs = { "fox1", "cat1", "cat2", "cat3", "fox2", "fox3" },
-    duration = 150,
+    npcs = { "fox1", "cat1", "cat3", "fox2", "frog1" },
+    duration = 120,
   }, {-- the sixth one
-  npcs = { "goat1", "fox1", "deer1", "deer2" },
-  duration = 180,
+  npcs = { "goat1", "fox1", "deer1", "frog2" },
+  duration = 150,
   }, {-- the finale
-    npcs = { "deer1", "goat1", "fox1", "cat1", "deer2", "cat1", "cat2", "cat3" },
-    duration = 300,
+    npcs = { "deer1", "goat1", "fox1", "cat1", "deer2", "frog1", "goat2", "fox2", "frog2" },
+    duration = 180,
   }}
 }
 
